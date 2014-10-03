1ab0e78bde67a75576e1d468338df97ed12f2b0b
Jamie from Desktop <jamiebclark@gmail.com>
2014-10-03 00:07:00 -0400

    Updated AppHelper helpers

--- View/Helper/LogDisplayAppHelper.php
+++ View/Helper/LogDisplayAppHelper.php
@@ -1,5 +1,7 @@
 <?php
 class LogDisplayAppHelper extends AppHelper {
+	public $helpers = array('Html');
+	
 	public function beforeRender($viewFile) {
 		$this->Html->css('LogDisplay.style', null, array('inline' => false));
 		$this->Html->script('LogDisplay.script', array('inline' => false));
