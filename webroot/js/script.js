a4508ff759ca756342aea8a1877f7638990594e1
Jamie from Desktop <jamiebclark@gmail.com>
2014-11-02 10:45:03 -0500

    Removed JS console calls

--- webroot/js/script.js
+++ webroot/js/script.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
 	$('.logfilecontent-more-toggle').each(function() {
 		$(this).click(function(e) {
-			console.log('CLICKED');
 			e.preventDefault();
 			
 			var $this = $(this),
 				targetId = $this.data('more-toggle-target'),
 				$target = $('#' + targetId);
-			console.log(targetId);
 			if ($target.length) {
 				if ($target.is(':hidden')) {
 					$target.slideDown();
